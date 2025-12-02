4cb12ab2cfb9f04ef5ae2b73c62f89f1bd421109
Daewis <davidabokunwa@gmail.com>
2025-12-02 02:15:31 +0100

    added cron job

---
+++ src/config/cron.js
@@ -0,0 +1,18 @@
+import { CronJob } from 'cron'; 
+import https from 'https';
+
+const job = new CronJob("*/14 * * * *", function () {
+    if (!process.env.API_URL) {
+        console.log("Skipping Keep-Alive: API_URL not set");
+        return;
+    }
+
+    https.get(process.env.API_URL, (res) => {
+        if (res.statusCode === 200) console.log("GET request sent successfully");
+        else console.log("GET request failed", res.statusCode);
+    })
+    .on("error", (e) => console.error("Error while sending request", e));
+
+});
+
+export default job;
